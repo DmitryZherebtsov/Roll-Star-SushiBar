160674001dd451c6af381779f884fbd147e96ac0
Dmity <zherebtsovdima31@gmail.com>
2024-08-11 17:58:38 +0200

    webCommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import NavBar from './components/NavBar/NavBar';
+import OpenCart from './components/OpenCart/OpenCart';
+import Footer from './components/Footer/Footer';
+import MenuPage from './components/MenuPage/MenuPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainPage from './components/MainPage/MainPage';
+import AboutUsPage from './components/AboutUsPage/AboutUsPage';
+import Reviews from './components/Reviews/Reviews';
+import CartProvider from './components/OpenCart/CartContext';
 
-function App() {
-  const [count, setCount] = useState(0)
+// import config from './config.js'
+
+const App = () => {
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          {/* <img src={viteLogo} className="logo" alt="Vite logo" /> */}
-        </a>
-        <a href="https://react.dev" target="_blank">
-          {/* <img src={reactLogo} className="logo react" alt="React logo" /> */}
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <CartProvider>
+        <Router>
+          <NavBar />
+
+          <Routes>
+    
+            <Route path='/' element={<MainPage />} />
+
+            <Route path='/MenuPage/:category?' element={<MenuPage />} />
+
+            <Route path='/OpenCart' element={<OpenCart />} />
+
+            <Route path='/AboutUsPage' element={<AboutUsPage />} />
+
+            <Route path='/Reviews' element={<Reviews />} />
+          
+          </Routes>
+
+          <Footer />
+          
+        </Router>
+      </CartProvider>
+    </div>
+  );
 }
 
-export default App
+export default App;
